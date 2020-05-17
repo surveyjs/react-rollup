9c67a1b744ab7d69e506ebca3f841690573b5266
dmitrykurmanov <kurmanov.work@gmail.com>
2020-05-17 21:11:52 +0300

    added survey-react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import styles from './styles.module.css'
+import * as Survey from 'survey-react'
+import 'survey-react/survey.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  const json = {
+    questions: [
+      {
+        type: 'radiogroup',
+        name: 'car',
+        title: 'What car are you driving ??',
+        isRequired: true,
+        colCount: 4,
+        choices: [
+          'None',
+          'Ford',
+          'Vauxhall',
+          'Volkswagen',
+          'Nissan',
+          'Audi',
+          'Mercedes-Benz',
+          'BMW',
+          'Peugeot',
+          'Toyota',
+          'Citroen'
+        ]
+      }
+    ]
+  }
+
+  const survey = new Survey.Model(json)
+
+  return (
+    <div>
+      <Survey.Survey model={survey} />
+      <div className={styles.test}>Example Component: {text}</div>
+    </div>
+  )
 }
